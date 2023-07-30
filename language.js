df4aa156aa725254595c7fd52c0bc34111ff529e
HibikiTatsuno <123159412+HibikiTatsuno@users.noreply.github.com>
2023-07-30 22:17:36 +0900

    定数の追加

---
+++ language.js
@@ -0,0 +1,22 @@
+export const LANGUAGES = [
+  {
+    language: "japanese",
+    code: "ja"
+  },
+  {
+    language: "english",
+    code: "en"
+  },
+  {
+    language: "korea",
+    code: "ko"
+  },
+  {
+    language: "china_CN",
+    code: "zh-CN"
+  },
+  {
+    language: "china_TW",
+    code: "zh-TW"
+  },
+]
--- translate.js
+++ translate.js
@@ -1,18 +1,17 @@
-require('dotenv').config();
-const {TranslationServiceClient} = require('@google-cloud/translate');
+import { LANGUAGES } from './language';
+
+async function translateText(targetText) {
+  require('dotenv').config();
+  const {TranslationServiceClient} = require('@google-cloud/translate');
+  const projectId = process.env.GCP_PROJECT_ID
+  const location = 'global'
 
 
-const projectId = process.env.GCP_PROJECT_ID
-const location = 'global'
-
-
-const translationClient = new TranslationServiceClient();
-
-async function translateText() {
+  const translationClient = new TranslationServiceClient();
   // Construct request
   const request = {
     parent: `projects/${projectId}/locations/${location}`,
-    contents: [targetTranslateText],
+    contents: [targetText],
     mimeType: 'text/plain', // mime types: text/plain, text/html
     sourceLanguageCode: 'ja',
     targetLanguageCode: 'ko',
@@ -21,9 +20,36 @@ async function translateText() {
   // Run request
   const [response] = await translationClient.translateText(request);
 
-  for (const translation of response.translations) {
-    console.log(`Translation: ${translation.translatedText}`);
+  const options = {
+    flag: "a"
   }
+
+  for (const translation of response.translations) {
+    const fs = require("fs")
+    fs.writeFileSync("./translateResult/korean_result.txt", translation.translatedText, options)
+  }
+
+  console.log("書き込み完了")
 }
 
-translateText();
+const inputPrompt = async () => {
+  const prompts = require("prompts");
+  let questions = {
+    type: "list",
+    name: "targetText",
+    message: "翻訳対象のテキストを入力してください"
+  };
+  const targetText =  await prompts(questions);
+
+  translateText(targetText.targetText);
+}
+
+// const outPutTranslateResult = (translatedText) => {
+//   const fs = require('fs')
+//   fs.writeFile("./translateResult/result.txt", translatedText, (error) => {
+//     console.log(`error`)
+//   })
+// }
+
+inputPrompt()
+
